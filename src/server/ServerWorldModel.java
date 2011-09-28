5702272e84fd033c4c3d9eaaadf3bf00d7dfbcc0
bloodrizer <cheshire.panther@gmail.com>
2011-09-28 18:48:31 +0400

    unrelevant changes

--- src/server/ServerWorldModel.java
+++ src/server/ServerWorldModel.java
@@ -5,9 +5,13 @@
 
 package server;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.lwjgl.util.Point;
 import world.WorldModel;
 import world.layers.WorldLayer;
+
 
 /**
  *
@@ -19,26 +23,50 @@ import world.layers.WorldLayer;
  */
 
 public class ServerWorldModel extends WorldModel{
+
     public ServerWorldModel(){
+
         //do some sql lite initialization there
         for (int i = 0; i< LAYER_COUNT; i++ ){
             WorldLayer layer = new WorldLayer(){
                 @Override
                 protected void build_chunk(Point origin, int z_index){
+                        //TODO: implement server-side chunk generation
 
-                    //TODO: implement server-side chunk generation
+                        //TODO: implement server-side chunk generation
+   
+                        //2. If not, perform generation cycle on it and create necesery entities
 
-                    //1. Load chunk from db and check if we did not generate it allready
+                        //2. If not, perform generation cycle on it and create necesery entities
 
-                    //2. If not, perform generation cycle on it and create necesery entities
+                        //3. Then load entities list from database
 
-                    //2b.If yes, perform terrain-only generation
-
-                    //3. Then load entities list from database
                 }
+                
+                
+                
+                @Override
+                public synchronized void chunk_gc(){
+                    //check if this chunk was not used for a long amt of time,
+                    //far from any player,  or we running low on memory
+                }
+                
+                public void unloadChunk(){
+                    //save chunk data (region standings or smth. like this)
+                    
+                    //1. save chunk entities
+                    
+                    
+                    
+                    //2. unload them
+                    //3. unload the chunk
+                }
+                
             };
             layer.set_zindex(i);
             worldLayers.put(i, layer);
         }
     }
 }
+
+//TODO: to be investigated - ehcache, Berkeley DB, DirectMemory/Redis
--- src/world/layers/WorldLayer.java
+++ src/world/layers/WorldLayer.java
@@ -285,6 +285,7 @@ public class WorldLayer{
             }
         }
     }
+    
 
 
 
